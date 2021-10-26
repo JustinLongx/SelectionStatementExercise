701879e2b59f515171f8e61b4f0f7a9b88b1d33c
justin long <justinxlong93@gmail.com>
2021-10-26 16:50:03 -0500

    initial commit

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 100);
+            int userInput;
+            do
+            {
+                Console.WriteLine("Try to guess my favorite number between 1 & 100");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too Lowwwww!");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too High!");
+                }
+                else if (userInput == favNumber)
+                {
+                    Console.WriteLine("That's correct!");
+                }
+                else
+                {
+                    Console.WriteLine("nevermind");
+                }
+            } while (favNumber != userInput);
+        }
+    }
+}
